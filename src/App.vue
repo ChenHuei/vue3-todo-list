6ba3f26051134100f9d9c9da5fc8c6462e41fd75
Leo <leo.chen@cloud-interactive.com>
2020-11-23 10:18:04 +0800

    Modify index page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>TODO LIST</h2>
+  <List />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import List from '@/components/List.vue'
 
-export default {
+export default defineComponent({
+  components: { List },
   name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    return {
+    }
   }
-}
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
