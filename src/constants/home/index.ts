05f1066317b1b546267674a2d19886e6c3c1fb1b
Leo <leo.chen@cloud-interactive.com>
2020-11-23 16:27:48 +0800

    Modify constants folder

---
+++ src/constants/home/index.ts
@@ -0,0 +1,18 @@
+interface Item {
+  id: number
+  name: string
+  isCompleted: boolean
+}
+
+export const LIST: Item[] = [
+  {
+    id: 0,
+    name: 'test1',
+    isCompleted: true,
+  },
+  {
+    id: 1,
+    name: 'test2',
+    isCompleted: false,
+  },
+];
