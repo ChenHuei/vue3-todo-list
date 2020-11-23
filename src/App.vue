ad0095270392e285e74276b1937e5d0062843d4a
Leo <leo.chen@cloud-interactive.com>
2020-11-23 10:35:23 +0800

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <h2>TODO LIST</h2>
-  <List />
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import List from '@/components/List.vue'
 
 export default defineComponent({
-  components: { List },
   name: 'App',
   setup () {
     return {
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>TODO LIST</h2>
+  <List />
+</template>
+
+<script>
+import List from './home/List'
+export default {
+  components: {List},
+  setup () {
+  
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
